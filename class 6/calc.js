b46108bb32e6960d387fdafe1f3677c559320c2e
Sarthak Nagapurkar <nsarthak99@gmail.com>
2025-08-03 19:22:58 +0530

    Initial commit

---
+++ class 6/calc.js
@@ -0,0 +1,28 @@
+let computeval = function(action) {
+        //Blank Validation
+        if (document.getElementById("first").value === '' | document.getElementById("second").value === '') {
+            document.getElementById("finalresult").innerHTML = "<strong>Incorrect Input</strong>"
+        }
+        else{
+            let first = parseInt(document.getElementById("first").value);
+            let second = parseInt(document.getElementById("second").value);
+            performCalculation(action, first, second);      
+        }
+}
+
+function performCalculation(action, first, second) {
+    if (action === "add") {
+        document.getElementById("finalresult").innerHTML = first + second;
+    }
+    else if (action === "sub") {
+        document.getElementById("finalresult").innerHTML = first - second;
+    }
+    else if (action === "multiply") {
+        document.getElementById("finalresult").innerHTML = first * second;
+    }
+}
+
+function submitForm(event){
+    event.preventDefult();
+    return false;
+}
---
+++ third/first.js
@@ -0,0 +1,11 @@
+let popalert = function(){
+    alert("Page has loaded");
+}
+
+function hoveroverfirst(){
+    document.getElementById("firstdiv").innerHTML="<p>This is the text</p>";
+}
+
+let adddata = (t) => document.getElementById("seconddiv")
+
+let updatetext = (t) => document.getElementById("seconddiv").innerHTML = t.value;
